2886c5eb98e290730969238d5f9112a559d92d69
Swmnx <No>
2021-07-09 22:33:48 +0300

    [Action] Entered url for searching feed;  [Action] Cell selected action

--- RSSReader/Screens/AddFeed/AddFeedController.swift
+++ RSSReader/Screens/AddFeed/AddFeedController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class AddFeedController: UIViewController {
+class AddFeedController: UIViewController, UITextFieldDelegate {
     
     class AddFeedInitParams {
         var add = false
@@ -16,22 +16,130 @@ class AddFeedController: UIViewController {
     }
 
     var params = AddFeedInitParams()
+    var rssService = DummyRssService()
+    
+    var foundCategories = [String]()
+    var rssFeedHasCategories = false
+    var loadedByDefault = false
     
     @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var categoriesView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if params.add {
-            self.title = "Add"
-            button.setTitle("Add", for: .normal)
-        } else {
-            self.title = "Edit"
-            button.setTitle("Edit", for: .normal)
+        searchField.delegate = self
+        categoriesView.delegate = self
+        categoriesView.dataSource = self
+        disableSaveButton()
+        let screenTitleText = params.add ? "Add" : "Edit"
+        self.title = screenTitleText
+        button.setTitle("Save", for: .normal)
+        if !params.add {
+            searchField.text = params.existedFeed?.url.absoluteString
+            foundCategories = params.existedFeed!.categories
+            loadedByDefault = true
+            categoriesView.reloadData()
         }
-        
-        // Do any additional setup after loading the view.
     }
+
+}
+
+//
+// MARK: Table view data source
+//
+extension AddFeedController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foundCategories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCellId", for: indexPath)
+        cell.textLabel?.text = foundCategories[indexPath.row]
+        return cell
+    }
+
+}
+
+//
+// MARK: Selection and Deselection actions
+//
+extension AddFeedController {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if !loadedByDefault && rssFeedHasCategories && tableView.indexPathsForSelectedRows != nil {
+            self.enableSaveButton()
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if !loadedByDefault && rssFeedHasCategories && tableView.indexPathsForSelectedRows == nil {
+            self.disableSaveButton()
+        }
+    }
+}
+
+//
+// MARK: Saving Action
+//
+extension AddFeedController {
+    
     @IBAction func buttonTapped(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
+    }
+}
+
+//
+// MARK: Search Action
+//
+extension AddFeedController {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField.text!.isEmpty {
+            return false
+        }
+        let url = URL.init(string: textField.text!)
+        guard let unwrappedUrl = url else {return false}
+        textField.resignFirstResponder()
+        rssService.searchFeed(url: unwrappedUrl, completionHandler: { [weak self] error, searchFeedResponse in
+            guard let response = searchFeedResponse else {
+                self?.rssFeedHasCategories = false
+                self?.disableSaveButton()
+                self?.foundCategories.removeAll()
+                let alertController = UIAlertController(title: error!, message: nil, preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil )
+                alertController.addAction(okAction)
+                self?.present(alertController, animated: true, completion: nil)
+                return
+            }
+            loadedByDefault = false
+            if response.categories.count > 0 {
+                self?.rssFeedHasCategories = true
+                self?.disableSaveButton()
+            } else {
+                self?.enableSaveButton()
+            }
+            self?.foundCategories = response.categories
+            self?.categoriesView.reloadData()
+        })
+       return true
+    }
+}
+
+//
+// MARK: Utility Methods
+//
+
+extension AddFeedController {
+    
+    private func disableSaveButton() {
+        button.isEnabled = false
+        button.backgroundColor = .gray
+    }
+    
+    private func enableSaveButton() {
+        button.isEnabled = true
+        button.backgroundColor = .systemBlue
     }
 }
--- RSSReader/Service/DummyRssService.swift
+++ RSSReader/Service/DummyRssService.swift
@@ -10,7 +10,16 @@ import Foundation
 class DummyRssService: RssServiceProtocol {
     
     func searchFeed(url: URL, completionHandler: (_ error: String?, _ data: SearchFeed?) -> Void) {
-        let searchFeed = SearchFeed.init(title: "Test Feed record", url: URL.init(string: "https://www.nasa.gov/rss/dyn/breaking_news.rss")!, categories: ["dd646"])
+        let searchFeed = SearchFeed.init(title: "Test Feed record", url: URL.init(string: "https://www.nasa.gov/rss/dyn/breaking_news.rss")!, categories: ["dd646", "dd", "dfsfs"])
         completionHandler(nil, searchFeed)
+    }
+    
+    func searchFeedWithoutCategories(url: URL, completionHandler: (_ error: String?, _ data: SearchFeed?) -> Void) {
+        let searchFeed = SearchFeed.init(title: "Test Feed record", url: URL.init(string: "https://www.nasa.gov/rss/dyn/breaking_news.rss")!, categories: [])
+        completionHandler(nil, searchFeed)
+    }
+    
+    func searchFeedWithError(url: URL, completionHandler: (_ error: String?, _ data: SearchFeed?) -> Void) {
+        completionHandler("dsss", nil)
     }
 }
