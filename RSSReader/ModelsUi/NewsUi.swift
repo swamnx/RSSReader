9e78a512ae3dd9d22c9c537d3d061770b4a66837
Swmnx <No>
2021-07-10 14:02:37 +0300

    [Structure] News

---
+++ RSSReader/ModelsUi/NewsUi.swift
@@ -0,0 +1,20 @@
+//
+//  NewsUi.swift
+//  RSSReader
+//
+//  Created by swamnx on 10.07.21.
+//
+
+import Foundation
+import UIKit
+
+struct NewsUi {
+    var id: Int
+    var icon: UIImage?
+    var title: String
+    var url: URL
+    var text: String
+    var read = false
+    var favourite = false
+}
+
